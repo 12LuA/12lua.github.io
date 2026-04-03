dae826539ee4b49abfc74b5075243321b48030ca
12LuA <71656192+12LuA@users.noreply.github.com>
2026-04-03 19:15:16 +0200

    feat: integrate Plausible analytics and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import { Geist, Geist_Mono } from "next/font/google"
 
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
+import PlausibleProvider from "next-plausible"
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'})
+const geist = Geist({ subsets: ["latin"], variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,10 +21,20 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", geist.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        geist.variable
+      )}
     >
       <body>
-        <ThemeProvider>{children} <div className='background-gradient' /></ThemeProvider>
+        <PlausibleProvider domain="analytics.12lua.de" selfHosted={true}>
+          <ThemeProvider>
+            {children}
+            <div className="background-gradient" />
+          </ThemeProvider>
+        </PlausibleProvider>
       </body>
     </html>
   )
